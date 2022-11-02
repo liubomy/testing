f351b7180a8584562702a7e435d55eae32b55e14
liubomy <36444313+liubomy@users.noreply.github.com>
2022-11-02 12:58:15 +0200

    Update db.php

--- src/crud/db.php
+++ src/crud/db.php
@@ -2,7 +2,7 @@
 session_start();
 
 $conn = mysqli_connect(
-  'jjjj-rdsdatabase-ax6jkz1wf8q5.cxxahj2wricq.us-east-2.rds.amazonaws.com',
+  'us-east-2.rds.amazonaws.com',
   'root',
   'password123',
   'php_mysql_crud'
